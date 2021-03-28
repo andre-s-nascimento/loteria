0c3c94fd0a74bf4f3d3574bc1e2936b17faf3e65
André Soares Nascimento <andresoaresnascimento@gmail.com>
2021-03-28 20:37:55 -0300

    Commit Inicial do projeto de jogo de loteria em Java para Web

---
+++ src/main/java/br/com/caelum/vraptor/controller/IndexController.java
@@ -0,0 +1,20 @@
+package br.com.caelum.vraptor.controller;
+
+import br.com.caelum.vraptor.*;
+
+@Controller
+public class IndexController {
+
+	
+	public IndexController() {
+		
+	}
+	
+	public void home() {
+		
+	}
+	
+	public void andre() {
+		
+	}
+}
