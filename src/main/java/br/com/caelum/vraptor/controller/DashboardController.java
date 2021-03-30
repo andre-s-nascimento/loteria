b5d0571929e6a0db05ff8ad4deaa3a2ab2a07d79
André Soares Nascimento <andresoaresnascimento@gmail.com>
2021-03-30 16:04:42 -0300

    Projeto BackEnd - Final

--- src/main/java/br/com/caelum/vraptor/controller/DashboardController.java
+++ src/main/java/br/com/caelum/vraptor/controller/DashboardController.java
@@ -6,10 +6,9 @@ import javax.persistence.EntityManager;
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
-import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.dao.UsuarioDAO;
-import br.com.caelum.vraptor.model.Usuario;
+
 
 @Path("dashboard")
 @Controller
--- src/main/java/br/com/caelum/vraptor/model/Usuario.java
+++ src/main/java/br/com/caelum/vraptor/model/Usuario.java
@@ -1,9 +1,6 @@
 package br.com.caelum.vraptor.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
